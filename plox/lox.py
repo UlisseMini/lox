8982793ffce25eb046552f7631af4d7f15b6ae31
Ulisse Mini <ulisse.mini@gmail.com>
2021-11-04 13:29:12 -0400

    Finish python lexer

--- plox/lox.py
+++ plox/lox.py
@@ -5,32 +5,20 @@ from typing import List, Callable, Any, Optional, Generator
 
 KEYWORDS = {'if', 'while'}
 
-class TokenType(Enum):
-    EOF = 0
-    IDENT = 1
-    LITERAL = 2
-    KEYWORD = 3
-    OP = 4
-    SYM = 5
-
-    def __str__(self):
-        return self.name
-
 @dataclass
 class Token:
     lexeme: str
-    type_: TokenType
     literal: Any = None
 
+    def __str__(self):
+        return self.lexeme
+
     def __bool__(self):
-        return self.type_ != TokenType.EOF
+        # eof is represented as an empty lexeme
+        return self.lexeme != ''
 
-    def __repr__(self):
-        if self.literal:
-            return f'T({self.lexeme}, {self.type_}, {self.literal})'
-        else:
-            return f'T({self.lexeme}, {self.type_})'
 
+# some predicates for parsing
 is_digit = lambda c: c in string.digits
 is_alpha = lambda c: c in string.ascii_letters+'_'
 is_alphanumeric = lambda c: is_digit(c) or is_alpha(c)
@@ -41,73 +29,81 @@ class Scanner:
         self.current = 0
         self.source = source
 
-    def scan(self):
-        while not self.at_end():
-            char = self.advance()
-            if is_digit(char): yield self.scan_number()
-            if is_alpha(char): yield self.scan_ident()
-            if char in '+-*/=!><': yield self.scan_op(char)
-            self.start = self.current
+    # ======== Helpers ========
 
-        return Token("", TokenType.EOF)
-
-    def scan_op(self, char: str):
-        two = char + self.peek()
-        if two in ['==', '!=', '>=', '<=']:
-            self.advance()
-            return self.emit(TokenType.OP)
-        else:
-            return self.emit(TokenType.OP)
-
-    def scan_number(self) -> Token:
-        self.advance_if(lambda x: x == '.')
-        self.advance_while(is_digit)
-        return self.emit(TokenType.LITERAL, float(self.lexeme()))
-
-    def scan_ident(self) -> Token:
-        self.advance_while(is_alphanumeric)
-        type_ = TokenType.KEYWORD if self.lexeme() in KEYWORDS else TokenType.IDENT
-        return self.emit(type_)
-
-    def advance_while(self, pred: Callable):
-        while pred(self.peek()): self.advance()
-
-    def advance_if(self, pred: Callable) -> Optional[str]:
-        if pred(self.peek()):
-            return self.advance()
-
-    def advance(self):
-        char = self.source[self.current]
-        self.current += 1
-        return char
-
-    def eat(self, want: str):
-        got = self.advance()
-        assert got == want, f'want {want} got {got}'
-
-    def peek(self):
-        return '\0' if self.at_end() else self.source[self.current]
+    def emit(self, literal=None) -> Token:
+        tok = Token(lexeme=self.lexeme(), literal=literal)
+        self.start = self.current
+        return tok
 
     def at_end(self):
         return self.current >= len(self.source)
 
+    def peek(self):
+        return '' if self.at_end() else self.source[self.current]
+
+    def advance(self):
+        char = self.peek()
+        self.current += 1
+        return char
+
     def lexeme(self):
         return self.source[self.start : self.current]
 
-    def emit(self, type_: TokenType, literal=None) -> Token:
-        tok = Token(lexeme=self.lexeme(), type_=type_, literal=literal)
-        self.start = self.current
-        return tok
+    def advance_if(self, pred: Callable) -> Optional[str]:
+        if not self.at_end() and pred(self.peek()):
+            return self.advance()
+
+    def advance_while(self, pred: Callable):
+        while self.advance_if(pred): pass
+
+    # ======== Scanning ========
+
+    def scan(self):
+        while char := self.advance():
+            if char == '"': yield self.scan_string()
+            if is_digit(char): yield self.scan_number()
+            if is_alpha(char): yield self.scan_ident()
+            if char in '+-*/=!><()}{;': yield self.scan_sym(char)
+            self.start = self.current
+
+        return Token('')
+
+    def scan_string(self):
+        self.advance_while(lambda c: c != '"')
+        assert not self.at_end(), 'eof before end of string'
+        self.advance()
+        return self.emit()
+
+    def scan_sym(self, char: str):
+        two = char + self.peek()
+        if two in {'==', '!=', '>=', '<='}:
+            self.advance()
+
+        return self.emit()
+
+    def scan_number(self) -> Token:
+        self.advance_if(lambda x: x == '.')
+        self.advance_while(is_digit)
+        return self.emit(literal=float(self.lexeme()))
+
+    def scan_ident(self) -> Token:
+        self.advance_while(is_alphanumeric)
+        return self.emit()
 
 
 tests = [
-    ("3*foo+5.2", "[T(3, LITERAL, 3.0), T(*, OP), T(foo, IDENT), T(+, OP), T(5.2, LITERAL, 5.2)]"),
-    ("foo", "[T(foo, IDENT)]"),
-    ("foo != !bar", "[T(foo, IDENT), T(!=, OP), T(!, OP), T(bar, IDENT)]"),
-    ("if(foo != bar)", "[T(if, KEYWORD), T(foo, IDENT), T(!=, OP), T(bar, IDENT)]"),
+    ("3*foo+5.2", '3 * foo + 5.2'),
+    ("foo != !bar", 'foo != ! bar'),
+    ("if(foo != bar)", 'if ( foo != bar )'),
+    ('if(foo != bar) { print "cats"; }', 'if ( foo != bar ) { print "cats" ; }'),
+    ('while(foo!=bar){print "dogs"; foo=bar+1;}',
+     'while ( foo != bar ) { print "dogs" ; foo = bar + 1 ; }'),
 ]
 
-for source, want_tokens in tests:
-    got_tokens = str(list(Scanner(source).scan()))
-    assert got_tokens == want_tokens, f'got {got_tokens} want {want_tokens}'
+for source, want in tests:
+    got_tokens = list(Scanner(source).scan())
+    got = ' '.join([str(tok) for tok in got_tokens])
+    assert want == got, f'got "{got}" want "{want}"'
 
+print(f'Tests passed')
